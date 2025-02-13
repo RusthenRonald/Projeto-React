8dfde38e3ccc4dbe16a6e210da5ae396896cd5b8
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-13 18:37:55 -0300

    adicionando inputs do form

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Frase from './components/frase';
 import Evento from './components/Evento';
+import Form from './components/Form';
 function App() {
   return (
     <div className="App">
+        <Form/>
         <Evento numero={1}/>
         <Evento numero={2}/>
     </div>
---
+++ meu-projeto/src/components/Form.js
@@ -0,0 +1,15 @@
+function Form(){
+    return(
+        <div>
+            <h1>Meu cadatro</h1>
+            <form>
+                <div>
+                    <input type="text" placeholder="Digite seu nome"></input>
+                </div>
+                <div>
+                    <input type="submit"></input>
+                </div>
+            </form>
+        </div>
+    )
+} export default Form
