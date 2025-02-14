c3bb2037aa0141df3280751d3b092cb3e81211da
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-14 02:44:54 -0300

    aplicando estilos no react

--- meu-projeto/src/components/frase.js
+++ meu-projeto/src/components/frase.js
@@ -1,9 +1,10 @@
 import SayMyName from "./SayMyName"
 import Pessoa from "./Pessoa"
+import styles from './Frase.module.css'
 function Frase(){
     return(
         <div>
-            <h1>Criando primeiro componente</h1>
+            <h1 className={styles.textContent}>Criando primeiro componente</h1>
             <SayMyName nome='rusthen'/>
             <SayMyName nome='josé'/>
             <Pessoa nome='jose' idade={14} profissão='gari'/>
