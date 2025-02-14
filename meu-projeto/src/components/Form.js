0a6442e8d82f46034b28e6412c82cdf1ccc00a71
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-13 21:06:23 -0300

    atualizei

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,19 +1,25 @@
+import {useState} from 'react'
+
 function Form(){
     function Enviar(event){
         event.preventDefault()
         console.log('Cadastrou usuário!')
     }
+
+    const {name,setName} = useState{}
+
+
     return(
         <div>
             <h1>Meu cadatro</h1>
             <form onSubmit={Enviar}>
                 <div>
-                    <label htmlFor='name'>Name</label>
+                    <label htmlFor='name'>Name:</label>
                     <input type="text" placeholder="Digite seu nome" id='name'></input>
                 </div>
                 <div>
-                    <label htmlFor="senha">Senha</label>
-                    <input type='password' placeholder='Digite sua senha' id='senha'></input>
+                    <label htmlFor="password">Senha:</label>
+                    <input type='password' placeholder='Digite sua senha' id='password'></input>
                 </div>
                 <div>
                     <input type="submit"></input>
