4f9126f96a49de9ddd0430eff91cb5cafa9bc376
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-14 02:04:56 -0300

    criando varias props

---
+++ meu-projeto/src/components/Pessoa.js
@@ -0,0 +1,7 @@
+function Pessoa({nome,idade,profissão}){
+    return( 
+        <>
+            <p>{nome} , {idade} , {profissão}</p>
+        </>
+    )
+} export default Pessoa
--- meu-projeto/src/components/frase.js
+++ meu-projeto/src/components/frase.js
@@ -1,10 +1,12 @@
 import SayMyName from "./SayMyName"
+import Pessoa from "./Pessoa"
 function Frase(){
     return(
         <div>
             <h1>Criando primeiro componente</h1>
             <SayMyName nome='rusthen'/>
             <SayMyName nome='josé'/>
+            <Pessoa nome='jose' idade={14} profissão='gari'/>
         </div>
     )
 }
