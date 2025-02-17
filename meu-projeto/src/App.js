43bf1dfc3337213cae1d4f565c2c94347a3ec145
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-16 23:00:39 -0300

    adicionando evento onclick

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Frase from './components/frase';
 import Evento from './components/Evento';
 import Form from './components/Form';
+import Evento2 from './components/Evento2';
 function App() {
   return (
     <div className="App">
-        
+        <h1>Testando Eventos</h1>
+        <Evento2/>
     </div>
   );
 }
---
+++ meu-projeto/src/components/Evento2.js
@@ -0,0 +1,8 @@
+function Evento2(){
+    return(
+        <div>
+            <p>Clique abaixo para disparar o evento</p>
+            <button>Ativar!</button>
+        </div>
+    )
+}export default Evento2
