66898dbef35c9308c073b5b9092204f3dcfb39af
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-16 23:24:08 -0300

    previnindo comportamento padrão do sistema

--- meu-projeto/src/components/Form2.js
+++ meu-projeto/src/components/Form2.js
@@ -1,14 +1,22 @@
 function Form2(){
+    function Cadastrar(e){
+        console.log('Cadastrou usuário!')
+        e.preventDefault()
+    }
     return(
-        <>
-            <div>
-                <label htmlFor='text'>Nome</label>
-                <input type='text' placeholder='Digite seu nome' id='text'></input> 
-            </div>
-            <div>
-                <label htmlFor='password'>Senha</label>
-                <input type='password' placeholder='Digite sua senha' id='password'></input>
-            </div>
-        </>
+            <form action="" onSubmit={Cadastrar}>
+                <div>
+                    <label htmlFor='text'>Nome</label>
+                    <input type='text' placeholder='Digite seu nome' id='text'></input>
+                </div>
+                <div>
+                    <label htmlFor='password'>Senha</label>
+                    <input type='password' placeholder='Digite sua senha' id='password'></input>
+                </div>
+                <div>
+                    <input type='submit' ></input>
+                </div>
+            </form>
+
     )
 } export default Form2
