7d77d3609e5b7df622f327d21f8e79f43fcc3c73
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-16 23:14:55 -0300

    adicionando htmlfor do label

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Frase from './components/frase';
 import Evento from './components/Evento';
 import Form from './components/Form';
+import Form2 from './components/Form2';
 function App() {
   return (
     <div className="App">
         <h1>Testando Eventos</h1>
+        <Form2/>
     </div>
   );
 }
--- meu-projeto/src/components/Form2.js
+++ meu-projeto/src/components/Form2.js
@@ -1,5 +1,13 @@
 function Form2(){
     return(
-
+        <>
+            <div>
+                <input type='text' placeholder='Digite seu nome'></input> 
+            </div>
+            <div>
+                <label htmlFor='password'>Senha</label>
+                <input type='password' placeholder='Digite sua senha' id='password'></input>
+            </div>
+        </>
     )
 } export default Form2
