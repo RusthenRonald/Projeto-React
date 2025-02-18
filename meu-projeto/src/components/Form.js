8870c761f602a3289fe1977d1df3aa6a3124f0f9
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 01:24:16 -0300

    deletei

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,30 +1,20 @@
-import {useState} from 'react'
+
 
 function Form(){
-    function Enviar(event){
-        event.preventDefault()
-        console.log(name)
-        console.log(password)
+    function Enviar(){
         console.log('Cadastrou usuário!')
     }
-
-    function Mudanca(e){
-        setName(e.target.value)
-    }
-    const [name,setName]=useState('rusthen')
-    const [password,setPassword]=useState()
-
     return(
         <div>
             <h1>Meu cadatro</h1>
             <form onSubmit={Enviar}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type="text" placeholder="Digite seu nome" id='name' onChange={Mudanca} ></input>
+                    <input type="text" placeholder="Digite seu nome" id='name' ></input>
                 </div>
                 <div>
                     <label htmlFor="password">Senha:</label>
-                    <input type='password' placeholder='Digite sua senha' id='password' onChange={(e)=>setPassword(e.target.value)}></input>
+                    <input type='password' placeholder='Digite sua senha' id='password'></input>
                 </div>
                 <div>
                     <input type="submit"></input>
