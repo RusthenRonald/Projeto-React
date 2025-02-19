efd4871f18d8db067bce48bfc6e406764031ad8c
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 22:24:14 -0300

    reaproveitando componente

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -3,10 +3,14 @@ function Evento(){
     function MeuEvento(){
         console.log(`Primeiro Evento!`)
     }
+    function SegundoEvento(){
+        console.log("Ativando o segundo evento!")
+    }
     return(
         <>
             <p>Clique para ativar o Evento</p>
             <Button event={MeuEvento} text='Primeiro evento'/>
+            <Button event={SegundoEvento} text='Segundo Evento!'/>
         </>
     )
 } export default Evento
