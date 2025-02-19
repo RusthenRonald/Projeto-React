b7fdc43e0362a068db9306fea7705de45604bbdf
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 22:58:19 -0300

    adicionando inputs e button com eventos

--- meu-projeto/src/components/Condicional.js
+++ meu-projeto/src/components/Condicional.js
@@ -4,6 +4,10 @@ function Condicional(){
     return(
         <div>
             <h2>Cadastre o seu e-mail</h2>
+            <form>
+                <input type='email' placeholder='Digite seu e-mail...'></input>
+                <butto onClick={EnviarEmail}>Enviar E-mail</butto>
+            </form>
         </div>
     )
 } export default Condicional
