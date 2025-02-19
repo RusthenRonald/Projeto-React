13737cee864cd356e2dfc92835be9bc5dc401857
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 17:01:10 -0300

    criando componente de lista

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import Condicional from './components/Condicional';
 import './App.css';
 function App() {
+
+  const meusItens = ['React' ,'Vue' ,'Angular']
+
   return (
     <div className="App">
-        <h1>Renderização Condicional</h1>
-        <Condicional/>
+        <h1>Renderização de Listas</h1>
     </div>
   );
 }
