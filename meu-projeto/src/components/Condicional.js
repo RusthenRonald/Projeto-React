29a35344eb304b3769f732d74a366cb7ec44ee64
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 23:13:49 -0300

    refazendo usando arrow function

--- meu-projeto/src/components/Condicional.js
+++ meu-projeto/src/components/Condicional.js
@@ -1,15 +1,19 @@
-import {usestate} from 'react'
+import {useState} from 'react'
 
 function Condicional(){
+
+    const [email,setEmail] =useState()
+
     function EnviarEmail(e){
         e.preventDefault()
         console.log("testando")
+        console.log(email)
     }
     return(
         <div>
             <h2>Cadastre o seu e-mail</h2>
             <form>
-                <input type='email' placeholder='Digite seu e-mail...'></input>
+                <input type='email' placeholder='Digite seu e-mail...' onChange={(e)=>setEmail(e.target.value)}></input>
                 <button type='submit' onClick={EnviarEmail}>Enviar E-mail</button>
             </form>
         </div>
