abf5fdf97a7d5250f018306d1b66269e1c1021ff
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 16:53:42 -0300

    deletando antigo exercicio

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import Condicional from './components/Condicional';
 import './App.css';
-import Form2 from './components/Form2';
 function App() {
   return (
     <div className="App">
         <h1>Renderização Condicional</h1>
-        <Form2/>
+        <Condicional/>
     </div>
   );
 }
