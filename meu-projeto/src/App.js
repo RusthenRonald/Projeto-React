16c6c1e184eb5adb11c86528b1e453cb2248e54d
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 14:43:47 -0300

    importando componente

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Condicional from './components/Condicional';
 import './App.css';
+import Form2 from './components/Form2';
 function App() {
   return (
     <div className="App">
         <h1>Renderização Condicional</h1>
+        <Form2/>
     </div>
   );
 }
--- meu-projeto/src/components/Form2.js
+++ meu-projeto/src/components/Form2.js
@@ -1,5 +1,7 @@
 function Form2(){
     return(
-
+        <div>
+            
+        </div>
     )
 } export default Form2
