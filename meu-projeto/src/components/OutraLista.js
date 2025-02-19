238538312861bb9ee3a6b84e423c12c35c52bea1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 17:28:00 -0300

    atualizei

---
+++ meu-projeto/src/components/OutraLista.js
@@ -0,0 +1,10 @@
+function OutraLista({itens}){
+    return(
+        <>
+            <h3>Lista de coisas boas</h3>
+            {
+                itens.map()
+            }
+        </>
+    )
+} export default OutraLista
