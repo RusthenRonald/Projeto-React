94dec84f41bd217c5e94cd468f48e43385551724
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 14:50:35 -0300

    importando usestate

--- meu-projeto/src/components/Form2.js
+++ meu-projeto/src/components/Form2.js
@@ -1,7 +1,12 @@
+import {useState} from 'react'
 function Form2(){
     return(
         <div>
-            
+            <form>
+                <label htmlFor="name">Nome </label>
+                <input type='text' placeholder="Digite seu nome" id='name'></input>
+                <button type='submit'>Enviar</button>
+            </form>
         </div>
     )
 } export default Form2
