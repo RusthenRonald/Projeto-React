36c83cb2de484b76552bbe534c6d583002ff4862
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 22:55:42 -0300

    chamando componente

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Condicional from './components/Condicional';
 import './App.css';
 function App() {
   return (
     <div className="App">
         <h1>Renderização Condicional</h1>
+        <Condicional/>
     </div>
   );
 }
--- meu-projeto/src/components/Condicional.js
+++ meu-projeto/src/components/Condicional.js
@@ -1,6 +1,9 @@
 import {usestate} from 'react'
+
 function Condicional(){
     return(
-
+        <div>
+            <h2>Cadastre o seu e-mail</h2>
+        </div>
     )
 } export default Condicional
