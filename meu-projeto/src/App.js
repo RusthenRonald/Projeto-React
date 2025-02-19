eb4c81eb2e99f54377feb8b1075d48ec9c435d28
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-18 22:52:28 -0300

    deletei

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Frase from './components/frase';
-import Evento from './components/Evento';
-import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-        <h1>Testando Eventos</h1>
-        <Evento/>
-
+        <h1>Renderização Condicional</h1>
     </div>
   );
 }
