a9ae2bde6039b6c3fe2a81a6bbd175b325e8a4c5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 23:12:01 -0300

    criando lista

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
 
-  const meusItens = ['React' ,'Vue' ,'Angular']
+  const meusItens =['React','Vue','Angular']
 
   return (
     <div className="App">
