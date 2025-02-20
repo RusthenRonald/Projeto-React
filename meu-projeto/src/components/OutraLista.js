098e8b47c980b863cae338e8eb7a1d0d178a172f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-19 23:41:18 -0300

    atualizei

--- meu-projeto/src/components/OutraLista.js
+++ meu-projeto/src/components/OutraLista.js
@@ -2,11 +2,8 @@ function OutraLista({itens}){
     return(
         <>  
             <h3>Lista de coisas boas</h3>
-            {
-                itens.map((item,index)=>(
-                    <p key={index}> {item}</p>
-                ))
-            }
+            {itens.map((itens,index)=>
+            <p key={index}>{itens}</p>)}
         </>
     )
 } export default OutraLista
