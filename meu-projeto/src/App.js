fc08a2226bd8646472590cc0879d000a8cb1801a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-21 15:35:41 -0300

    deletando para refazer renderização de listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import OutraLista from './components/OutraLista';
 function App() {
-
-  const meusItens =['React','Vue','Angular']
 
   return (
     <div className="App">
         <h1>Renderização de Listas</h1>
-        <OutraLista itens={meusItens}/>
-        <OutraLista itens={[]}/>
     </div>
   );
 }
