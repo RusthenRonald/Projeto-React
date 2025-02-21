436bee791c778595eb9ed84312221cc179df5172
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-21 16:47:47 -0300

    deletei

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Lista from './components/Lista';
 function App() {
 
-  const minhalista= ['React','Vue','Js']
 
   return (
     <div className="App">
         <h1>Renderização de Listas</h1>
-        <Lista itens={minhalista}/>
-        <Lista itens={[]}/>
     </div>
   );
 }
