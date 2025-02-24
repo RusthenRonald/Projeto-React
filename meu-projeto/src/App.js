e2eeec62bd2ce89b81ae80fe3d070444b4c6e3d6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 14:35:10 -0300

    instalando e importando react router dom

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,8 +1,12 @@
+import {browserRouter as Router, Routes ,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 function App() {
 
   return (
+    <div>
+
+    </div>
   );
 }
 
