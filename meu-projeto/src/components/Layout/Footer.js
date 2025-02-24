8152f3e2bcf4b10f51d2f70145d7b35cef082d30
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 16:55:29 -0300

    estilos para footer

--- meu-projeto/src/components/Layout/Footer.js
+++ meu-projeto/src/components/Layout/Footer.js
@@ -1,10 +1,14 @@
 import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.modules.css'
 function Footer(){
     return(
-        <div>
-            <FaFacebook/>
-            <FaInstagram/>
-            <FaLinkedin/>
-        </div>
+        <footer>
+            <ul className={styles.social-list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p>Nosso Rodapé</p>
+        </footer>
     )
 } export default Footer
