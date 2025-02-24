f058a0994fb8426668e456460c3b88dd268dd749
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 14:28:53 -0300

    revendo conceitos de rotas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,32 +1,8 @@
-import {BrowserRouter as Router , Routes, Route ,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Pages/Home';
-import Empresa from './components/Pages/Empresa';
-import Contato from './components/Pages/Contato';
 function App() {
 
   return (
-    <Router>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/empresa'>Empresa</Link>
-        </li>
-        <li>
-          <Link to='/contato'>Contato</Link>
-        </li>
-      </ul>
-
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/empresa' element={<Empresa/>}></Route>
-          <Route path='/contato' element={<Contato/>}></Route>
-        </Routes>
-
-    </Router>
   );
 }
 
