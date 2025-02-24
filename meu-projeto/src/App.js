79915d3cc9a7bb85b98066de0ecea6b1b98c45b5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 23:17:12 -0300

    importando componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,16 @@
 import {BrowserRouter as Router , Switch , Route ,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Pages/Home';
+import Empresa from './components/Pages/Empresa';
+import Contato from './components/Pages/Contato';
 function App() {
 
   return (
     <Router>
       <ul>
         <li>
-          <Link to='/home'>Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
           <Link to='/empresa'>Empresa</Link>
