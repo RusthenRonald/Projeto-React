1f8a7ce7e58a5042b99ff057e4f4fb23ee5ae721
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 21:13:46 -0300

    revendo conceito de listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,16 +1,10 @@
-import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-import SeuNome from './components/SeuNome';
 function App() {
-
-  const [nome,setNome] =useState()
 
   return (
     <div className="App">
-        <h1>State Lift</h1>
-        <SeuNome setNome={setNome}/>
-        {nome}
+      <h1>Renderizando Listas</h1>
     </div>
   );
 }
