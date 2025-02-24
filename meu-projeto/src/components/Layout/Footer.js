9a02922d20dedadd202b8d8690498298e7541758
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 17:04:30 -0300

    aplicando estilos nos icones

--- meu-projeto/src/components/Layout/Footer.js
+++ meu-projeto/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 function Footer(){
     return(
         <footer>
-            <ul className={styles.social-list}>
+            <ul className={styles.social_list}>
                 <li><FaFacebook/></li>
                 <li><FaInstagram/></li>
                 <li><FaLinkedin/></li>
