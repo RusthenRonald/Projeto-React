fb5cfeae2097d7c1b3934faf7f7d238c3f2868b9
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 14:44:39 -0300

    linkando paginas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,18 @@
-import {browserRouter as Router, Routes ,Route,Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes ,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <li>Empresa</li>
+        <li>Contato</li>
+      </ul>
+    </Router>
   );
 }
 
