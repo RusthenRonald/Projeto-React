649c4607490d99398d5b46377819bfede9bb4230
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 17:00:18 -0300

    renomeei pasta

--- meu-projeto/src/components/Layout/Footer.js
+++ meu-projeto/src/components/Layout/Footer.js
@@ -1,5 +1,5 @@
 import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
-import styles from './Footer.modules.css'
+import styles from './Footer.module.css'
 function Footer(){
     return(
         <footer>
