e978305dfc6f67a531ad4392b6076703e730cc26
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 22:56:10 -0300

    apagando usestate

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,19 +1,11 @@
 import {BrowserRouter as Router , Switch , Route ,Link} from 'react-router-dom'
-import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
 function App() {
-
-  const [nome,setNome]=useState()
 
   return (
     <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      {nome}
-      <Saudacao nome={nome}/>
+      <h1>React Router</h1>
     </div>
   );
 }
