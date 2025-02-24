fdd6ae0af1bd1e752199d984ff950f194b6466a6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-24 14:50:50 -0300

    importando componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,6 +1,9 @@
 import {BrowserRouter as Router, Routes ,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Pages/Home';
+import Empresa from './components/Pages/Empresa';
+import Contato from './components/Pages/Contato';
 function App() {
 
   return (
