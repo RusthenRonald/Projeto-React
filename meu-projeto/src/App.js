218862698a4d33722a060a546a7dcd8e94ff53f5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 21:50:08 -0300

    apagando componente

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import OutraLista from './components/OutraLista';
+
 function App() {
 
-  const minhalista =['React','Vue','Js']
+
 
   return (
     <div className="App">
-      <h1>Renderizando Listas</h1>
-      <OutraLista Lista={minhalista}/>
-      <OutraLista Lista={[]}/>
+      <h1>State Lift</h1>
     </div>
   );
 }
