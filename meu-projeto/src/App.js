0a1c71a4d173d4d48d6d4acd96b67c981e3fb9a1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 21:16:13 -0300

    criando componente de lista

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
 
+  const minhalista =['React','Vue','Js']
+
   return (
     <div className="App">
       <h1>Renderizando Listas</h1>
