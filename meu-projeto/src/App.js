72c0d1dee514ab1dd0ca359efc09cd1244e06b74
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-23 21:57:11 -0300

    importando useState

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,15 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import SeuNome from './components/SeuNome';
 function App() {
 
-
+  
 
   return (
     <div className="App">
       <h1>State Lift</h1>
+      <SeuNome/>
     </div>
   );
 }
--- meu-projeto/src/components/SeuNome.js
+++ meu-projeto/src/components/SeuNome.js
@@ -1,5 +1,8 @@
 function SeuNome(){
     return(
-
+        <div>
+            <p>Digite o seu nome</p>
+            <input type={"text"} placeholder="Digite seu nome"></input>
+        </div>
     )
-} export default SeuNomea
+} export default SeuNome
