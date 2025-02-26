06a805272ac39f23da026031aa5206ffc0894c27
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 17:14:16 -0300

    botando conteudo da navbar

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -4,15 +4,12 @@ import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/Layout/Container";
+import NavBar from "./components/Layout/NavBar";
+import Footer from "./components/Layout/Footer";
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Company</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/newproject'>NewProject</Link>
-      </div>
+      <NavBar/>
 
       <Container customClass="min_height">
         <Routes>
@@ -22,7 +19,7 @@ function App() {
           <Route path="/newproject" element={<NewProject/>}></Route>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
