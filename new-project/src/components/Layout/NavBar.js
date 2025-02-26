548b1badf445c0aa392224b56ddd3636fe249bbd
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 17:38:26 -0300

    estilizando cada item da navbar

--- new-project/src/components/Layout/NavBar.js
+++ new-project/src/components/Layout/NavBar.js
@@ -5,24 +5,26 @@ import styles from './NavBar.module.css'
 import logo from '../../img/costs_logo.png'
 function NavBar(){
     return(
-        <Container>  
-            <Link to='/'>
-            <img src={logo}></img></Link>
-            <ul className={styles.navbar}>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/company'>Company</Link>
-                </li>
-                <li>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-                <li>
-                    <Link to='/newproject'>NewProject</Link>
-                </li>
-            </ul>
-        </Container>
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/'>
+                <img src={logo}></img></Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link to='/' className={styles.item_a}>Home</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/company'  className={styles.item_a}>Company</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/contact'  className={styles.item_a}>Contact</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/newproject'  className={styles.item_a}>NewProject</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
 
     )
 } export default NavBar
