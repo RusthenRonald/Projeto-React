67ed2b4e6c830006ac09d63acf61e2a001c5124f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 15:25:36 -0300

    aplicando estilos

--- new-project/src/components/Layout/Container.js
+++ new-project/src/components/Layout/Container.js
@@ -2,6 +2,6 @@ import styles from './Container.module.css'
 
 function Container(props){
     return(
-        <div>{props.children}</div>
+        <div className={styles.container}>{props.children}</div>
     )
 } export default Container
