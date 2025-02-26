e52ed3419258f7a8bb06ed09c090432ec143d3c9
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 20:21:19 -0300

    importando estilos do footer

--- new-project/src/components/Layout/Footer.js
+++ new-project/src/components/Layout/Footer.js
@@ -1,11 +1,13 @@
 import {FaFacebook,FaInstagram,FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
 function Footer(){
     return(
-        <footer>
+        <footer className={styles.footer}>
             <ul>
                 <li><FaFacebook/></li>
                 <li><FaInstagram/></li>
                 <li><FaLinkedin/></li>
+                <p><span>Costs</span>&copy; 2025</p>
             </ul>
         </footer>
     )
