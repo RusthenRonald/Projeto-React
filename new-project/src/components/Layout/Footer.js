d0926d135676e4acb3c43398c11e4f236fe5b367
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 17:28:51 -0300

    fazendo estilo para navbar

--- new-project/src/components/Layout/Footer.js
+++ new-project/src/components/Layout/Footer.js
@@ -1,5 +1,5 @@
 function Footer(){
     return(
-        <div></div>
+        <div><p>footer</p></div>
     )
 } export default Footer
--- new-project/src/components/Layout/NavBar.js
+++ new-project/src/components/Layout/NavBar.js
@@ -5,12 +5,24 @@ import styles from './NavBar.module.css'
 import logo from '../../img/costs_logo.png'
 function NavBar(){
     return(
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/company'>Company</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/newproject'>NewProject</Link>
-        </div>
+        <Container>  
+            <Link to='/'>
+            <img src={logo}></img></Link>
+            <ul className={styles.navbar}>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/company'>Company</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+                <li>
+                    <Link to='/newproject'>NewProject</Link>
+                </li>
+            </ul>
+        </Container>
 
     )
 } export default NavBar
