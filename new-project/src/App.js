7422bba1832d9d59fca1011b7660b71d37d279c8
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-25 22:31:49 -0300

    criando componentes e definindo rotas

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -2,7 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
-      
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/company" element={<Company/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/newproject" element={<NewProject/>}></Route>
+      </Routes>
+      <p>Footer</p>
     </Router>
   );
 }
