bef9e14a97ad51c97c6b2f550be24ed9b1423431
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-26 21:29:37 -0300

    importando img

--- new-project/src/components/pages/Home.js
+++ new-project/src/components/pages/Home.js
@@ -7,6 +7,7 @@ function Home(){
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <a href='/'>Criar Projeto</a>
+            <img src={savings}></img>
         </section>
     )
 }export default Home
