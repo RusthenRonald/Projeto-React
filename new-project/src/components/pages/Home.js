0a4a3363ea4847f82a4cc3894756c66aac2a5a5a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-27 20:41:32 -0300

    usando react router

--- new-project/src/components/pages/Home.js
+++ new-project/src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home(){
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <LinkButton to='NewProject' />
+            <LinkButton to='NewProject' text='Novo Projeto' />
             <img src={savings}></img>
         </section>
     )
