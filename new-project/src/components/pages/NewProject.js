06c02344c7270819f33a056db82dffddd09c7913
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-27 22:20:03 -0300

    importando css

--- new-project/src/components/pages/NewProject.js
+++ new-project/src/components/pages/NewProject.js
@@ -1,8 +1,10 @@
+import styles from './NewProject.module.css'
 function NewProject(){
     return(
         <div>
             <h1>Criar Seu Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>formulário</p>
         </div>
     )
 }export default NewProject
