170053005f93097bb032519bc61acaeab6f16434
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-27 22:39:18 -0300

    envolvi inputs por div para quebrar linha

--- new-project/src/components/pages/NewProject.js
+++ new-project/src/components/pages/NewProject.js
@@ -1,10 +1,11 @@
 import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
 function NewProject(){
     return(
         <div className={styles.new_project_container}>
             <h1>Criar Seu Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>formulário</p>
+            <ProjectForm/>
         </div>
     )
 }export default NewProject
--- new-project/src/components/project/ProjectForm.js
+++ new-project/src/components/project/ProjectForm.js
@@ -1,7 +1,18 @@
 function ProjectForm(){
     return(
-        <div>
-
-        </div>
+        <form>
+            <div>
+                <input type="text" placeholder="Insira um nome do projeto"></input>
+            </div>
+            <div>
+                <input type='number' placeholder="Insira o orçamento total"></input>
+            </div>
+            <select name="category_id">
+                <option disabled>Selecione a categoria</option>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+        </form>
     )
 } export default ProjectForm
