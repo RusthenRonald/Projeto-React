c2fc8269be94f5c7dd980ed70f469db2d9cb9600
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-27 22:23:52 -0300

    estilizando página

--- new-project/src/components/pages/NewProject.js
+++ new-project/src/components/pages/NewProject.js
@@ -1,7 +1,7 @@
 import styles from './NewProject.module.css'
 function NewProject(){
     return(
-        <div>
+        <div className={styles.new_project_container}>
             <h1>Criar Seu Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
             <p>formulário</p>
