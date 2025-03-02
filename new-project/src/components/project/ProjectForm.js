2cf48acc234763d55d105db3969c3494e3267b90
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-02 00:18:57 -0300

    atualizei

--- new-project/src/components/project/ProjectForm.js
+++ new-project/src/components/project/ProjectForm.js
@@ -1,8 +1,19 @@
+import {useState} from 'react'
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import Submit from '../form/Submit'
 function ProjectForm(props){
+
+    const [categories,setCategories] = useState([])
+
+    fetch("http://localhost:5000/categories",{
+        method:"GET",
+        headers:{
+            'Content-Type': 'application/json'//determinando tipo da comunicação
+        }
+    })//obtendo e consumindo dados da api 
+
     return(
         <form className={styles.form}>
             <Input
