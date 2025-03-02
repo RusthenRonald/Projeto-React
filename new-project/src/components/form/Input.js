ddfbad94958810397e1c620bf97342679762a96a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-01 21:49:56 -0300

    adicionei props aos seus lugares

--- new-project/src/components/form/Input.js
+++ new-project/src/components/form/Input.js
@@ -1,9 +1,9 @@
 import styles from './Input.module.css'
 function Input({type,text,name,placeholder,handleOnchange,value}){
     return(
-        <div>
-            <label htmlFor=''></label>
-            <input type='text'></input>
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}</label>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnchange} value={value}></input>
         </div>
     )
 } export default Input
