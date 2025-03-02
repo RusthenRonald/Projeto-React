729b0739f89445be7504ab6d786d69c3fcd57ea1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-01 22:41:24 -0300

    estilizando botão

--- new-project/src/components/form/Submit.js
+++ new-project/src/components/form/Submit.js
@@ -2,7 +2,7 @@ import styles from './Submit.module.css'
 function Submit({text}){
     return(
         <div>
-            <button>{text}</button>
+            <button className={styles.bnt}>{text}</button>
         </div>
     )
 } export default Submit
