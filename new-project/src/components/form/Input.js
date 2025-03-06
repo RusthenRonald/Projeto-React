168f9ba03345752abffb958d7e181a931dbe8436
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-06 01:06:30 -0300

    atualizando input

--- new-project/src/components/form/Input.js
+++ new-project/src/components/form/Input.js
@@ -3,7 +3,7 @@ function Input({type,text,name,placeholder,handleOnchange,value}){
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
-            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnchange} value={value}></input>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnchange} value={value}></input> 
         </div>
-    )
+    ) //Isso significa que a prop handleOnchange do Input está recebendo a função handleChange do ProjectForm.
 } export default Input
