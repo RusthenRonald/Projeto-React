1955930566762b21df1af10c7ea209f28f863edb
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-10 23:33:56 -0300

    importando mensagem no project

--- new-project/src/components/Layout/Message.js
+++ new-project/src/components/Layout/Message.js
@@ -1,4 +1,6 @@
 import styles from './Message.module.css'
-function Message(){
-    return()
+function Message({type, msg}){
+    return(
+        <p>Mensagem</p>
+    )
 } export default Message
--- new-project/src/components/pages/Projects.js
+++ new-project/src/components/pages/Projects.js
@@ -1,8 +1,9 @@
+import Message from "../Layout/Message"
 function Projects (){
     return(
         <div>
             <h1>Meus Projetos</h1>
-            
+            <Message/>
         </div>
     )
 } export default Projects
