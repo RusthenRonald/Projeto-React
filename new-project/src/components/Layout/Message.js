667580dbc02dac46f7fe5f12e4c6e6101dab7e92
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-11 17:15:49 -0300

    criando função que ativa msg por 3 segundo e faz estado ser falso apos isso

--- new-project/src/components/Layout/Message.js
+++ new-project/src/components/Layout/Message.js
@@ -5,6 +5,25 @@ function Message({type, msg}){
 
     const[visible,setVisible]=useState(false)
 
+
+    useEffect(()=>{
+
+        if(!msg){
+            setVisible(false)
+            return
+        }
+        //senão
+        setVisible(true)
+
+        const timer = setTimeout(()=>{
+            setVisible(false) 
+        },3000) // apos 3 segundos a visibilidade vai ser falsa e a msg vai sumir
+
+        return ()=> clearTimeout(timer)
+    },[msg])
+
+    //Se visible === true, o <div> será renderizado.
+    //Se visible === false, nada será renderizado.
     return(
         <>
             {
