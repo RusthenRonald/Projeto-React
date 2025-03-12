2665dda8de461ef7d67cd9da21ace097fb4050cf
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-12 01:15:17 -0300

    adicionando comentários para fluxo da message

--- new-project/src/components/pages/Projects.js
+++ new-project/src/components/pages/Projects.js
@@ -1,12 +1,12 @@
 import {useLocation} from "react-router-dom"
 import Message from "../Layout/Message"
 function Projects (){
-
-    const location=useLocation()
+    
+    const location=useLocation() // useLocation() para recuperar os dados passados no state
     let message=''
     if(location.state){
         message=location.state.message
-    }
+    } //Se o state existir, o valor da chave message (que foi passado na navegação) é extraído e atribuído à variável message.
 
     return(
         <div>
