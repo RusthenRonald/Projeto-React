021b2512243d4a44fcbc067c75f1b3301adef826
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-19 02:05:06 -0300

    atualizei

--- new-project/src/components/pages/ProjectCard.js
+++ new-project/src/components/pages/ProjectCard.js
@@ -4,7 +4,7 @@ import {BsPencil,BsFillTrashFill} from "react-icons/bs"
 function ProjectCard ({id,name,budget,category,handleremove}){
     return(
         <div>
-            <p>Projeto</p>
+            <p>{name}</p>
         </div>
     )
 } export default ProjectCard
