a2051e1491fa3605097ea68140207896e363ceda
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-27 18:01:02 -0300

    adicionando props

--- new-project/src/components/project/ProjectCard.js
+++ new-project/src/components/project/ProjectCard.js
@@ -1,7 +1,7 @@
 import styles from "./ProjectCard.module.css"
 import {BsPencil,BsFillTrashFill} from "react-icons/bs"
 
-function ProjectCard(){
+function ProjectCard({id,name,budget,category,handleRemove}){
     return (
         <div>
             <p>Projeto</p>
