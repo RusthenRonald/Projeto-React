d453520616bbcfe2a571fa0622714f2919dfc9b0
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-28 02:21:13 -0300

    criando nova rota

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route path="/company" element={<Company/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
           <Route path="/newproject" element={<NewProject/>}></Route>
+          <Route path="/project" element={<Project/>}></Route>
         </Routes>
       </Container>
       <Footer/>
---
+++ new-project/src/components/pages/Project.js
@@ -0,0 +1,8 @@
+import styles from './Project.module.css'
+function Project(){
+    return(
+        <div>
+            <p>Projeto</p>
+        </div>
+    )
+} export default Project
