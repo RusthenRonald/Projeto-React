a2849ad196ba30e45ba85c4d8bd232aedbb90713
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-29 02:28:50 -0300

    fazendo condição pra exibir o loading

--- new-project/src/components/pages/Project.js
+++ new-project/src/components/pages/Project.js
@@ -1,6 +1,7 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState ,useEffect} from 'react'
+import Loading from '../Layout/Loading'
 
 function Project(){
     //pegando id pela url
@@ -10,22 +11,28 @@ function Project(){
     const [project,setProject]=useState([])
 
     useEffect(()=>{
-        fetch(`http://localhost:5000/projects/${id}`,{
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json"
-            },
-        }).then((resp)=>resp.json())
-        .then((data)=>{
-            setProject(data)
-            console.log(data)
-        })
-        .catch((err)=>console.log(err))
+        setTimeout(()=>{
+            fetch(`http://localhost:5000/projects/${id}`,{
+                method:"GET",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+            }).then((resp)=>resp.json())
+            .then((data)=>{
+                setProject(data)
+                console.log(data)
+            })
+            .catch((err)=>console.log(err))
+        },5000)
     },[id])
 
     return(
-        <div>
-            <p>{project.name}</p>
-        </div>
+        <>
+            {project.name ? (
+                <p>{project.name}</p>
+            ):(
+                <Loading/>
+            )}
+        </>
     )
 } export default Project
