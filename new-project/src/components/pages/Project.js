7ebda396cfd2718f73b2b5dd6a0b6d7cd9c441cb
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-04-29 02:06:29 -0300

    retornando dados da api

--- new-project/src/components/pages/Project.js
+++ new-project/src/components/pages/Project.js
@@ -1,8 +1,28 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
-
+import { useState ,useEffect} from 'react'
 
 function Project(){
+    //pegando id pela url
+    const {id} = useParams()
+    console.log(id)
+
+    const [project,setProject]=useState([])
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/projects/${id}`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            },
+        }).then((resp)=>resp.json())
+        .then((data)=>{
+            setProject(data)
+            console.log(data)
+        })
+        .catch((err)=>console.log(err))
+    },[id])
+
     return(
         <div>
             <p>Projeto</p>
