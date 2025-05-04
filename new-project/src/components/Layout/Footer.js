cbdde659f6cfe7ec7303a43a6fff3ea07bd6ce98
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-05-04 20:20:07 -0300

    substitui icone

--- new-project/src/components/Layout/Footer.js
+++ new-project/src/components/Layout/Footer.js
@@ -1,11 +1,11 @@
-import {FaFacebook,FaInstagram,FaLinkedin} from 'react-icons/fa'
+import {FaGithub,FaInstagram,FaLinkedin} from 'react-icons/fa'
 import styles from './Footer.module.css'
 function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li><a href=""><FaFacebook/></a></li>
-                <li><a href=""><FaInstagram/></a></li>
+                <li><a href=""><FaGithub /></a></li>
+                <li><a href="https://www.instagram.com/___rusthenronald/"><FaInstagram/></a></li>
                 <li><a href="https://www.linkedin.com/in/rusthen-ronald-631ba6300/" target='blank'><FaLinkedin/></a></li>
             </ul>
             <p className={styles.copy_right}><span>Costs</span>&copy; 2025</p>
