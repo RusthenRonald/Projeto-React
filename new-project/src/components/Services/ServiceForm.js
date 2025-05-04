c001de9e3ae9009e786879647e86d584ed5643a6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-05-04 17:33:12 -0300

    estruturando serviceform reutilizando componentes de input e estilos

--- new-project/src/components/Services/ServiceForm.js
+++ new-project/src/components/Services/ServiceForm.js
@@ -1,7 +1,40 @@
 import styles from "../project/ProjectForm.module.css"
+import { useState } from "react"
+import Input from "../form/Input"
+import Submit from "../form/Submit"
 
-function ServiceForm(){
-    <div>Service</div>
+function ServiceForm({handleSubmit,btnText,projectData}){
+    function submit(){
+
+    }
+
+    function handleChange(e){
+
+    }
+    return (
+        <form action="" className={styles.form} onSubmit={submit}>
+            <Input type="text"
+            text="Nome do serviço"
+            name="name"
+            placeholder="Insira o nome do serviço"
+            handleOnchange={handleChange}
+            />
+            <Input type="number"
+            text="Custo do Serviço"
+            name="cost"
+            placeholder="Insira o valor total"
+            handleOnchange={handleChange}
+            />
+            <Input type="text"
+            text="Descrição do serviço"
+            name="description"
+            placeholder="Descreva do serviço"
+            handleOnchange={handleChange}
+            />
+
+            <Submit text={btnText}/>
+        </form>
+    )
 } 
 
 export default ServiceForm
