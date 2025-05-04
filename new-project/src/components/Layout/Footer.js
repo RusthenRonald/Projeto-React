c9a57932024f0f489d6d4de93ddc43f9a271a188
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-05-04 20:17:31 -0300

    redirecionando para minhas redes sociais

--- new-project/src/components/Layout/Footer.js
+++ new-project/src/components/Layout/Footer.js
@@ -4,9 +4,9 @@ function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li><FaFacebook/></li>
-                <li><FaInstagram/></li>
-                <li><FaLinkedin/></li>
+                <li><a href=""><FaFacebook/></a></li>
+                <li><a href=""><FaInstagram/></a></li>
+                <li><a href="https://www.linkedin.com/in/rusthen-ronald-631ba6300/" target='blank'><FaLinkedin/></a></li>
             </ul>
             <p className={styles.copy_right}><span>Costs</span>&copy; 2025</p>
         </footer>
