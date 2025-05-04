30ebb8cd6296935a25f28c86172ed3cbf952d53c
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-05-04 20:29:06 -0300

    adicionando hover e alterando footer

--- new-project/src/components/Layout/Footer.js
+++ new-project/src/components/Layout/Footer.js
@@ -4,11 +4,11 @@ function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li><a href=""><FaGithub /></a></li>
-                <li><a href="https://www.instagram.com/___rusthenronald/"><FaInstagram/></a></li>
+                <li><a href="https://github.com/RusthenRonald" target='blank'><FaGithub /></a></li>
+                <li><a href="https://www.instagram.com/___rusthenronald/" target='blank'><FaInstagram/></a></li>
                 <li><a href="https://www.linkedin.com/in/rusthen-ronald-631ba6300/" target='blank'><FaLinkedin/></a></li>
             </ul>
-            <p className={styles.copy_right}><span>Costs</span>&copy; 2025</p>
+            <p className={styles.copy_right}><span>Rusthen Ronald</span>&copy; 2025</p>
         </footer>
     )
 } export default Footer
